118b4db35f6b9b250a9d8301c8e3734eaf892ad9
ninomarlou <ninomarlougonzales@gmail.com>
2022-03-29 00:55:13 +0100

    Solution for problem 14 - longest collatz sequence.

---
+++ 14-longest-collatz-sequence.py
@@ -0,0 +1,42 @@
+def main():
+
+    x = generate_collatz_sequence(1000000)
+    print(x)
+
+
+def generate_collatz_sequence(n):
+    cache = {}
+    start_points = range(1, n)
+    max_length = 0
+    longest_chain = []
+    collatz_sequence = map(
+        lambda x: collatz_sequence_generator_with_cache(x, cache), start_points)
+    for sequence in collatz_sequence:
+        if sequence[1] > max_length:
+            max_length = sequence[1]
+            longest_chain = sequence[0]
+    return longest_chain
+
+
+def collatz_sequence_generator_with_cache(n, cache):
+    key = n
+    n = float(n)
+    result = [n]
+    while n != 1:
+        if n in cache:
+            if key not in cache:
+                cache[key] = result[1:] + cache.get(n)
+            result = result + cache.get(n)
+            return [result[0], len(result)]
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = (3 * n) + 1
+        result.append(n)
+    if key not in cache:
+        cache[key] = result[1:]
+    return [result[0], len(result)]
+
+
+if __name__ == "__main__":
+    main()
